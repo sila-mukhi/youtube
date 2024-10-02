91e90fd6dd2587cca710cd7fe76bec92803d2543
Sila Mukhi <silamukhi165@gmail.com>
2024-10-02 12:01:49 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,103 @@
-const express= require ('express');
-const cors=require("cors");
+const express = require('express');
+const cors = require("cors");
 require('./db/config');
-const User=require("./db/user");
-const app=express();  
+const User = require("./db/user");
+const Product = require("./db/product");
+const product = require('./db/product');
+
+
+const app = express();
 app.use(express.json());  //middleware
-app.use(cors()); 
+app.use(cors());
 // app.post("/register",(req,res)=>{
 //     res.send(req.body)
 // }) ///used to acess data from postman
 
-app.post("/register",async (req,res)=>{
-    let user =new User(req.body); 
-    let result=await user.save();
-    result=result.toObject();
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
     delete result.password
-    res.send(result)
-
-}) 
-
-app.post("/login", async(req,resp)=>{
-    console.log(req.body);
-    if(req.body.password && req.body.email){
-        let user= await User.findOne(req.body).select("-password");
-    if(user){
-        resp.send(user)
-    }
-    else{
-        resp.send({result:"No User Found"})
-    }
-    }
-    else{
-        resp.send({result:"No User Found"})
-    }
-    
+    resp.send(result);
    
 })
+
+app.post("/login", async (req, resp) => {
+    console.log(req.body);
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+           resp.send(user)
+        }       
+                  
+        else {
+            resp.send({ result: "No User Found" })
+        }
+    }
+    else {
+        resp.send({ result: "No User Found" })
+    }
+
+
+})
+
+app.post("/add-product", async (req, resp) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result)
+})
+
+app.get("/products", async (req, resp) => {
+
+    let products = await product.find();
+    if (products.length > 0) {
+        resp.send(products)
+    }
+    else {
+        resp.send({ result: "No Products Found" })
+    }
+})
+
+app.delete("/product/:id", async (req, resp) => {
+    const result = await Product.deleteOne({ _id: req.params.id })
+    resp.send(result);
+});
+
+app.get("/product/:id", async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        resp.send(result)
+    }
+    else {
+        resp.send({ result: "No Record Found." })
+    }
+})
+
+
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    resp.send(result)
+});
+
+app.get("/search/:key",  async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } },
+            { price: { $regex: req.params.key } }
+        ]
+    });
+    resp.send(result)
+})
+
+
 
 
 app.listen(5000);
